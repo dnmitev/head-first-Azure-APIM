53b7b60570a48d20aa4fc5064bf79c0a09e85abd
Dimitar Mitev <dnmitev@gmail.com>
2018-05-16 12:30:26 +0300

    added exception controller to the ASP.NET Core part; added additional policies

---
+++ FitnessTracker/FitnessTracker/Controllers/ExceptionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessTracker.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/exception")]
+    public class ExceptionController : Controller
+    {
+        [HttpGet]
+        [Route("GetDate_RandomException")]
+        public async Task<IActionResult> GetDateWithRandomExceptions()
+        {
+            var now = DateTime.Now;
+            if (now.Second % 3 == 0)
+            {
+                throw new ArgumentException("Meaningless exception for the sake of the demo");
+            }
+
+            return Ok(DateTime.Now);
+        }
+    }
+}
